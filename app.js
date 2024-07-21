26360d189ca8bae0f87e6aaaa907f94a8396a950
SAWANT TUSHAR <sawanttushar796@gmail.com>
2024-07-21 21:33:22 +0530

    change

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const express =require('express');
+const express = require("express");
 const app = express();
-const path = require('path')
+const path = require("path");
 
+const userModel = require("./models/user");
 
-app.set('view engine',"ejs")
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/',(req,res)=>{
-    res.send('Hello World') 
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/read", async (req, res) => {
+  let users = await userModel.find();
+  res.render("read", { users });
+});
+app.post("/create", async (req, res) => {
+  console.log(req.body)
+  let { name, email, image } = req.body
+  let createdUser = await userModel.create({
+    name,
+    email,
+    image,
+  })
+
+  res.send(createdUser);
+});
 
 app.listen(3000);
