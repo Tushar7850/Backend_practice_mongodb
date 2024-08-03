413e981c9eb4f4f9f7785e6b2d1debed90cc7a4a
SAWANT TUSHAR <sawanttushar796@gmail.com>
2024-08-03 20:43:24 +0530

    change

--- app.js
+++ app.js
@@ -1,40 +1,109 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser");
-const path = require("path");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const userModel = require("./models/user");
 
-app.set("view engine", "ejs");
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cookieParser());
+const userModel = require("./models/user");
+const postModel = require("./models/post");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.send("hii");
 });
 
-app.post("/create", (req, res) => {
-  let { username, email, password, age } = req.body;
-
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(password, salt, async (err, hash) => {
-      let createdUser = await userModel.create({
-        username,
-        email,
-        password : hash,
-        age,
-      });
-
-      
-      res.send(createdUser);
-    });
+app.get("/create", async function (req, res) {
+  let user = await userModel.create({
+    username: "Tushar",
+    age: 25,
+    email: "Tush@gmail.com",
   });
+
+  res.send(user);
+});
+
+app.get("/post/create", async function (req, res) {
+  let post = await postModel.create({
+    postdata: "Hello ",
+    user: "66ae480b11c45172b61e2d36",
+  });
+
+  let user = await userModel.findOne({ _id: "66ae480b11c45172b61e2d36" });
+  if (!user) {
+    return res.status(404).send({ error: "User not found" });
+  }
+
+  console.log('user',user.posts);
+  
+    user.posts.push(post._id);
+  await user.save();
+
+  res.send({ post, user });
 });
 
 app.listen(3000);
+
+// -----------------------*****************User Login / Logout *************************----------------------------------
+
+// const express = require("express");
+// const app = express();
+// const cookieParser = require("cookie-parser");
+// const path = require("path");
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// const userModel = require("./models/user");
+
+// app.set("view engine", "ejs");
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(cookieParser());
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+// app.post("/create", (req, res) => {
+//   let { username, email, password, age } = req.body;
+
+//   bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, async (err, hash) => {
+//       let createdUser = await userModel.create({
+//         username,
+//         email,
+//         password: hash,
+//         age,
+//       });
+
+//       let token = jwt.sign({ email }, "shhhhhhhhhhhhhhhhh");
+//       res.cookie("token", token);
+//       res.send(createdUser);
+//     });
+//   });
+// });
+
+// app.get('/login',function (req,res) {
+//   res.render('login')
+
+// })
+// app.post('/login',async function (req,res) {
+// let user = await  userModel.findOne({email:req.body.email})
+// if (!user) return res.send("something went Wrong")
+//   console.log(user);
+
+// bcrypt.compare(req.body.password,user.password,function(err,result){
+//   if(result){
+//     let token = jwt.sign({ email :user.email }, "shhhhhhhhhhhhhhhhh");
+//     res.cookie("token", token);
+//     res.send('Yes your are login ')
+//   }
+//     else res.send("something went Wrong")
+// })
+
+// })
+
+// app.get('/logout',(req,res)=>{
+//   res.cookie('token',"")
+//   res.redirect('/')
+// })
+
+// app.listen(3000);
 
 // -------------------------cookieParser ----------------------------
 
@@ -153,4 +222,4 @@ app.listen(3000);
 //   res.redirect('/read')
 // });
 
-// app.listen(3000);
+// app.listen(3000)
--- models/user.js
+++ models/user.js
@@ -1,16 +1,40 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/authtestapp')
+mongoose.connect('mongodb://127.0.0.1:27017/testingthedatabase')
 
 
 const userSchema = mongoose.Schema({
     username:String,
     email:String,
-    password:String,
-    age:Number
+    age:Number,
+    posts:[
+        {type : mongoose.Schema.Types.ObjectId}
+    ],
 })
 
 module.exports = mongoose.model('user',userSchema);
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose')
+
+// mongoose.connect('mongodb://127.0.0.1:27017/authtestapp')
+
+
+// const userSchema = mongoose.Schema({
+//     username:String,
+//     email:String,
+//     password:String,
+//     age:Number
+// })
+
+// module.exports = mongoose.model('user',userSchema);
 
 
 
